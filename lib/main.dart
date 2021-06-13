57c25741c88c861d5cbcfb584f7168f46c06ad32
Erich <ecruz@bachecubano.com>
2021-06-13 02:02:12 -0400

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qvapay/src/pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,15 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'QvaPay',
+      home: Home(),
     );
   }
 }
---
+++ lib/src/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to QvaPay'),
+        ),
+        body: Column(
+          children: [
+            // Current Balance Card
+            // Some promo and cards/services
+            // Latest transactions
+          ],
+        ));
+  }
+}
