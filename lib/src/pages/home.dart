026918bf3f334e0fc1b1e97796c762947aa1afb7
Erich <ecruz@bachecubano.com>
2021-06-25 00:21:51 -0400

    User clic

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -41,8 +41,9 @@ class _HomePageState extends State<HomePage> {
         children: [
           Column(
             children: [
+              
               // Top Bar
-              topbar,
+              topbar(context),
 
               // Horizontal Menu
               Expanded(
@@ -154,87 +155,6 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
-        ],
-      ),
-    );
-  }
-
-  // Cards with transactons list
-  Container buildCategoryCard(
-      IconData icon, String title, int amount, int percentage) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-      ),
-      height: 85,
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    icon,
-                    color: Color(0xFF00B686),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Text(
-                    "\$$amount",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "($percentage%)",
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey),
-                  )
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Stack(
-            children: [
-              Container(
-                height: 5,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2),
-                    color: Colors.grey.shade300),
-              ),
-              Container(
-                height: 5,
-                width: 80,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2),
-                    color: Color(0XFF00B686)),
-              ),
-            ],
-          )
         ],
       ),
     );
--- lib/src/snippets/topbar.dart
+++ lib/src/snippets/topbar.dart
@@ -1,74 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-Widget topbar = Container(
-  height: 100,
-  child: Padding(
-    padding: const EdgeInsets.only(left: 15.0, right: 20.0, top: 42),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
+Widget topbar(BuildContext context) {
+  return Container(
+    height: 100,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 15.0, right: 20.0, top: 42),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Menú Icon
+              IconButton(
+                  icon: FaIcon(FontAwesomeIcons.bars),
+                  onPressed: () {
+                    print("Pressed");
+                  }),
+              // Profile details
+              profileData(context),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+// Hi, Name and Profile picture
+Widget profileData(BuildContext context) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.pushNamed(context, '/settings');
+    },
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Menú Icon
-            IconButton(
-                icon: FaIcon(FontAwesomeIcons.bars),
-                onPressed: () {
-                  print("Pressed");
-                }),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+        RichText(
+          text: TextSpan(
+              text: "Hola, ",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white38,
+                fontWeight: FontWeight.w400,
+              ),
               children: [
-                RichText(
-                  text: TextSpan(
-                      text: "Hola, ",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white38,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      children: [
-                        TextSpan(
-                            text: "\Erich!",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ))
-                      ]),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  width: 48.0,
-                  height: 48.0,
-                  decoration: BoxDecoration(
-                    color: Color(0XFF00B686),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(.2),
-                          blurRadius: 8,
-                          spreadRadius: 3)
-                    ],
-                    border: Border.all(
-                      width: 1.6,
+                TextSpan(
+                    text: "\Erich!",
+                    style: TextStyle(
+                      fontSize: 20,
                       color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(24.0),
-                  ),
-                  padding: EdgeInsets.all(0),
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://qvapay.com/storage/profiles/xGnoyrlZMy10Ta5hCQGvEtj6aqJK3Fa1rueU1lPv.jpg"),
-                  ),
-                ),
-              ],
+                      fontWeight: FontWeight.bold,
+                    ))
+              ]),
+        ),
+        SizedBox(width: 10),
+        Container(
+          width: 48.0,
+          height: 48.0,
+          decoration: BoxDecoration(
+            color: Color(0XFF00B686),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(.2),
+                  blurRadius: 8,
+                  spreadRadius: 3)
+            ],
+            border: Border.all(
+              width: 1.6,
+              color: Colors.white,
             ),
-          ],
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+          padding: EdgeInsets.all(0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://qvapay.com/storage/profiles/xGnoyrlZMy10Ta5hCQGvEtj6aqJK3Fa1rueU1lPv.jpg"),
+          ),
         ),
       ],
     ),
-  ),
-);
+  );
+}
